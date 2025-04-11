785250a9a6eb90ea6e135e049576403837ded226
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2025-04-12 00:03:24 +0530

    added http res code

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
 const net = require("net");
+const fs = require("fs");
+const path = require("path");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -25,6 +27,39 @@ const server = net.createServer((socket) => {
         if (user_agent) {
             const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${user_agent.length}\r\n\r\n${user_agent}`;
             socket.write(response);
+            return;
+        }
+
+
+        // if the path starts with /files/{filename} then return the file content
+        //         First request
+        // The first request will ask for a file that exists in the files directory:
+
+        // $ echo -n 'Hello, World!' > /tmp/foo
+        // $ curl -i http://localhost:4221/files/foo
+        if (path.startsWith("/files/")) {
+            const text = path.split("/")[2] || "";
+            const filePath = path.join(__dirname, `/tmp/${text}`);
+            // check if the file exists
+            fs.access(filePath, fs.constants.F_OK, (err) => {
+                if (err) {
+                    const response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    socket.write(response);
+                    return;
+                }
+
+                // read the file and return the content
+                fs.readFile(filePath, "utf8", (err, data) => {
+                    if (err) {
+                        const response = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+                        socket.write(response);
+                        return;
+                    }
+
+                    const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${data.length}\r\n\r\n${data}`;
+                    socket.write(response);
+                });
+            });
             return;
         }
 
